97e272d701ca74c1edbfa49a9d2446c58a4f9e55
ruhtar <arthur.amorim10@gmail.com>
2024-08-04 10:08:33 -0300

    feat: adds go dot env dependency

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	fmt.Println(os.Getenv("TEST"))
 }

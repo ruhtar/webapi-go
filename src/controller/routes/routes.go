e9c4a70003048d9147726c83a861d0d37b193f03
ruhtar <arthur.amorim10@gmail.com>
2024-08-04 10:48:14 -0300

    feat: adds route handlers

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -1,11 +1,14 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ruhtar/webapi-go/src/controller"
+)
 
 func InitRoutes(r *gin.RouterGroup) {
-	r.GET("/users/:userId")
-	r.GET("/users/email/:email")
-	r.POST("/users")
-	r.PUT("/users/:userId")
-	r.DELETE("users/:userId")
+	r.GET("/users/:userId", controller.FindUserById)
+	r.GET("/users/email/:email", controller.FindUserByEmail)
+	r.POST("/users", controller.CreateUser)
+	r.PUT("/users/:userId", controller.UpdateUser)
+	r.DELETE("users/:userId", controller.DeleteUser)
 }

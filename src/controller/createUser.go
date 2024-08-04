c57d86c998de445eb95ed805c8ea0ccd7b9c141d
ruhtar <arthur.amorim10@gmail.com>
2024-08-04 12:14:50 -0300

    feat: adds username validation

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,21 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ruhtar/webapi-go/src/configuration/rest_err"
+	"github.com/ruhtar/webapi-go/src/controller/dtos/request"
+)
 
 func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
 
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		rest_err := rest_err.NewBadRequestError(fmt.Sprintf("Incorrect fields, erro=%s", err.Error()))
+		c.JSON(rest_err.Code, rest_err)
+		return
+	}
+
+	c.JSON(200, userRequest)
 }
